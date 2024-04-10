60e70b16bc7ac5a491df0798e81fc5446ffda65b
miffy0803 <160700995+miffy0803@users.noreply.github.com>
2024-04-10 14:46:49 +0800

    a

---
+++ project-title/animal/data.js
@@ -0,0 +1,551 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -1.4970401289570852,
+        "pitch": 0.13739009442420524,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9066266219260903,
+          "pitch": 0.40270654395105154,
+          "rotation": 12.566370614359176,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "廣場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -1.491997923688384,
+        "pitch": 0.23661627373058636,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3702321948063982,
+          "pitch": 0.4826256636355595,
+          "rotation": 0.7853981633974483,
+          "target": "2-_"
+        },
+        {
+          "yaw": -2.2053585103925055,
+          "pitch": 0.3352542842562922,
+          "rotation": 6.283185307179586,
+          "target": "3-"
+        },
+        {
+          "yaw": -1.7443201219084514,
+          "pitch": 0.32083585542099513,
+          "rotation": 0,
+          "target": "8-"
+        },
+        {
+          "yaw": -1.837905858945419,
+          "pitch": 0.6592582939188674,
+          "rotation": 9.42477796076938,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-_",
+      "name": "廣場直走_熊貓館",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 2.8648728339620595,
+        "pitch": 0.25891337284718574,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0967658541388765,
+          "pitch": 0.7277762719477909,
+          "rotation": 3.141592653589793,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "飲食區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -0.3224064018845567,
+        "pitch": 0.23645764655384127,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.31577109647530754,
+          "pitch": 0.5692534755786607,
+          "rotation": 0.7853981633974483,
+          "target": "4-"
+        },
+        {
+          "yaw": -0.5739565370446797,
+          "pitch": 0.8154910937173465,
+          "rotation": 3.141592653589793,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "紀念品商店",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -1.8278377796957894,
+        "pitch": 0.4062292051812513,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3652909216871087,
+          "pitch": 0.1975375984423593,
+          "rotation": 0.7853981633974483,
+          "target": "5-"
+        },
+        {
+          "yaw": -2.8703132996780827,
+          "pitch": 0.44806156700592226,
+          "rotation": 3.9269908169872414,
+          "target": "3-"
+        },
+        {
+          "yaw": -2.2409285903050336,
+          "pitch": 0.7306818750593109,
+          "rotation": 1.5707963267948966,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "猴子區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -2.9283432486821788,
+        "pitch": 0.40554598498405525,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6118266380834925,
+          "pitch": 0.2714917515822606,
+          "rotation": 6.283185307179586,
+          "target": "6-"
+        },
+        {
+          "yaw": 2.899383589959207,
+          "pitch": 0.7654062958217374,
+          "rotation": 2.356194490192345,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-",
+      "name": "小岔路",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 2.206909543226856,
+        "pitch": 0.39132792717052567,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4296183042825756,
+          "pitch": 0.6790383676355134,
+          "rotation": 1.5707963267948966,
+          "target": "7-1_"
+        },
+        {
+          "yaw": 1.5943117475427169,
+          "pitch": 0.7195077243308319,
+          "rotation": 3.141592653589793,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-1_",
+      "name": "小岔路西1_休息區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -1.6266960908501478,
+        "pitch": 0.21333500631251567,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6558820466925948,
+          "pitch": 0.5291618417395583,
+          "rotation": 0,
+          "target": "11-"
+        },
+        {
+          "yaw": -1.2315980008306617,
+          "pitch": 0.7757183990655889,
+          "rotation": 7.853981633974483,
+          "target": "7-1_"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-",
+      "name": "涼亭",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -0.08618589504016683,
+        "pitch": 0.18092134509806712,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6100937701621465,
+          "pitch": 0.38296200181072315,
+          "rotation": 10.995574287564278,
+          "target": "4-"
+        },
+        {
+          "yaw": -0.2992259199233409,
+          "pitch": 0.1541396867993683,
+          "rotation": 5.497787143782138,
+          "target": "11-"
+        },
+        {
+          "yaw": 0.539579782820585,
+          "pitch": 0.24161737878840128,
+          "rotation": 0.7853981633974483,
+          "target": "9-"
+        },
+        {
+          "yaw": 0.10595524381759702,
+          "pitch": 0.6673749369359747,
+          "rotation": 2.356194490192345,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-",
+      "name": "不知名神社",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 2.7716110921230888,
+        "pitch": 0.23457695790856192,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.944111730741322,
+          "pitch": 0.5281614582159477,
+          "rotation": 4.71238898038469,
+          "target": "10-"
+        },
+        {
+          "yaw": -2.7279660904051717,
+          "pitch": 0.5785471224813978,
+          "rotation": 1.5707963267948966,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-",
+      "name": "鳥區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.10253596476349358,
+          "pitch": 0.6086089295220951,
+          "rotation": 3.141592653589793,
+          "target": "9-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-",
+      "name": "象區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1400512365374098,
+          "pitch": 0.12815647233240668,
+          "rotation": 0,
+          "target": "7-1_"
+        },
+        {
+          "yaw": -0.13031828105861543,
+          "pitch": 0.5627138224035377,
+          "rotation": 3.141592653589793,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
